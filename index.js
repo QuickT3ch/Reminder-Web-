ed56dc384e0dce88c93615fb7ffa97bed1baa66a
sarband <sarband2001muhamad@gmail.com>
2022-05-07 23:29:30 +0300

    added cookies for saving data for client side

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
+
 $(document).ready(()=>{
-   console.log("ready");     
-});
+
+//on page loading add already added reminders to the list of my reminders
+if ($.cookie("reminders")) {
+    let reminders = JSON.parse($.cookie("reminders"));
+    console.log(reminders);
+    reminders.forEach(reminder => {
+        $("#myreminders").append("<div class='d-flex gap-4'>  <input type='radio'>  <p class='reminder'>"+reminder.detail+"</p> <p>requierd date: "+reminder.date+"</p> </div>");
+    });
+} 
+
+
+});    
+    
+
+ 
 
 
 $("#addReminder").on("click", 
 ()=>{
     let detail = $("#reminderDetail").val();
     let dateAndTime = $("#reminderDate").val();
+
+ 
 if(!detail || !dateAndTime){
     alert("pleasee fill all the required fields");
 }
 else{
-    $("#reminderDetail").val("");
-    $("#reminderDate").val("");
-
-    $("#myreminders").append("<div class='d-flex gap-4'><button>  <input type='radio'> </button>   <p class='reminder'>"+detail+"</p> <p>requierd date: "+dateAndTime+"</p> </div>");
+       // seperate(split) date and time to their own vars
+       let dateAndTImeS = dateAndTime.split("T");
+       let date = dateAndTImeS[0];
+       let time = dateAndTImeS[1];
+       
+        //add to myreminders list on the screen
+        $("#reminderDetail").val("");
+        $("#reminderDate").val("");
+        $("#myreminders").append("<div class='d-flex gap-4'>  <input type='radio'>  <p class='reminder'>"+detail+"</p> <p>requierd date: "+dateAndTime+"</p> </div>");
+        // add to cookie
+         addToCookie(detail,date,time);
 }
 }
 
 );
+
+
+
+// function of addToCookie
+
+function addToCookie(detail,date,time){
+    let check = $.cookie("reminders");
+    //checking if its first time or not
+    if(check != undefined){
+        //not first time
+        let reminders = JSON.parse($.cookie("reminders"));
+        let newreminder = {"detail":detail, "date":date, "time":time ,"status":false};
+        reminders.push(newreminder);
+       $.cookie("reminders",JSON.stringify(reminders));
+    }else{
+
+        //first time
+        let cookie = [{"detail":detail, "date":date, "time":time ,"status":false}];
+        $.cookie("reminders",JSON.stringify(cookie));
+    }
+
+}
