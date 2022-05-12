adad0feb8a1a728ac79dff3ec8beefe262495358
sarband <sarband2001muhamad@gmail.com>
2022-05-12 23:28:28 +0300

    action buttons with done action fuctionality

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+var reminders;
 
 $(document).ready(()=>{
-
 //on page loading add already added reminders to the list of my reminders
 if ($.cookie("reminders")) {
-    let reminders = JSON.parse($.cookie("reminders"));
-    console.log(reminders);
-    reminders.forEach(reminder => {
-        $("#myreminders").append("<div class='d-flex gap-4'>  <input type='radio'>  <p class='reminder'>"+reminder.detail+"</p> <p>requierd date: "+reminder.date+"</p> </div>");
-    });
+    loadContent();
 } 
 
 
@@ -33,10 +29,11 @@ else{
         //add to myreminders list on the screen
         $("#reminderDetail").val("");
         $("#reminderDate").val("");
-        $("#myreminders").append("<div class='d-flex gap-4'>  <input type='radio'>  <p class='reminder'>"+detail+"</p> <p>requierd date: "+date+time+"</p> </div>");
         // add to cookie
          addToCookie(detail,date,time);
-}
+         $("#myreminders").empty();
+            loadContent();
+        }
 }
 
 );
@@ -61,4 +58,30 @@ function addToCookie(detail,date,time){
         $.cookie("reminders",JSON.stringify(cookie));
     }
 
+}
+
+
+$(document).on('click', '.ss', function (e) {
+    // your function here
+    reminders[this.id].status = true;   
+    $.cookie("reminders",JSON.stringify(reminders));
+    $("#myreminders").empty();
+    loadContent();
+});
+
+
+
+// loading content
+
+var loadContent = ()=>{
+ reminders = JSON.parse($.cookie("reminders"));
+
+    reminders.forEach((reminder,index) => {
+        if (reminder.status == false) {
+            $("#myreminders").append("<div class='reminders d-flex  flex-column align-items-center justify-content-center'> <p class='reminder'>"+reminder.detail+"</p> <p>requierd date: "+reminder.date+"</p> <p> required time: "+reminder.time+"</p> <div class='d-flex actions'><i class='ss bi bi-check2' id='"+index+"'></i><i class='bi bi-pencil'></i> <i class='bi bi-trash'></i></div> </div>");
+        }else{
+            $("#myreminders").append("<div class='reminders done d-flex  flex-column align-items-center justify-content-center'> <p class='reminder'>"+reminder.detail+"</p> <p>requierd date: "+reminder.date+"</p> <p> required time: "+reminder.time+"</p> <div class='d-flex actions'> <i class='bi bi-trash'></i></div> </div>"); 
+        }
+      
+    });
 }
